f648124bc3d3eb54904c4bc15246f20a6f63d6b3
BerserkTears <kirill.kunitsyn@gmail.com>
2023-02-01 16:27:05 +0300

    fix: Tape class working

--- lib/Tape.h
+++ lib/Tape.h
@@ -5,8 +5,6 @@
 #ifndef YADRO_TEST_TASK_TAPE_H
 #define YADRO_TEST_TASK_TAPE_H
 
-#include <cstdio>
-
 template <typename T>
 class Tape {
 private:
@@ -24,6 +22,32 @@ public:
 
     T Read();
 };
+
+template<typename T> Tape<T>::Tape(int n){
+    size = n;
+    current = 0;
+    data = new T[size];
+};
+
+template<typename T> Tape<T>::~Tape(){
+    free(data);
+}
+
+template<typename T> void Tape<T>::Write(T information){
+    data[current] = information;
+}
+
+template<typename T> void Tape<T>::Move(int steps){
+    current += steps;
+    if (current > size - 1)
+        throw std::out_of_range("Out of range of Tape");
+    if (current < 0)
+        throw std::out_of_range("Out of range of Tape");
+}
+
+template<typename T> T Tape<T>::Read(){
+    return data[current];
+}
 
 
 #endif //YADRO_TEST_TASK_TAPE_H
--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <Tape.h>
-
+#include "Tape.h"
 
 int main() {
     Tape<int> tape(7);
     tape.Write(4);
     tape.Move(1);
-//    tape.Write(50);
-//    tape.Move(-1);
-//    std::cout << tape.Read();
-//    std::cout << " ";
-//    tape.Move(1);
-//    std::cout << tape.Read();
-//    std::cout << " ";
+    tape.Write(50);
+    tape.Move(-1);
+    std::cout << tape.Read();
+    std::cout << " ";
+    tape.Move(1);
+    std::cout << tape.Read();
+    std::cout << " ";
 
     return 0;
 }
